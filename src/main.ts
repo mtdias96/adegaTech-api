476ce403480c0f5b0a2f4c3faf53f2c469043100
mtdias96 <matheusdias.front96@gmail.com>
2025-01-04 13:52:37 -0300

    feat: main/add use global guards

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter';
 
@@ -8,6 +9,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalGuards();
 
   await app.listen(process.env.PORT);
 }
