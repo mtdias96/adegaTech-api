d615be2ca41b7a68cd9b3644b7f43f83886ef3d8
mtdias96 <matheusdias.front96@gmail.com>
2025-01-04 13:50:46 -0300

    feat: ActiveUserId/add custom decoartor extracts the active user ID and checks if it is active

---
+++ src/shared/decorator/ActiveUserId.ts
@@ -0,0 +1,17 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+export const ActiveUserId = createParamDecorator<undefined>(
+  (data, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    const userId = request['userId'];
+
+    if (!userId) {
+      throw new UnauthorizedException('User not found');
+    }
+    return userId;
+  },
+);
