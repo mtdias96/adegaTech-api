af4807823c2ee898857d46531ec4ee7fe71a095d
mtdias96 <matheusdias.front96@gmail.com>
2025-01-05 14:56:13 -0300

    feat: multer/add multer and configure image upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AdministrativeModule } from './modules/administrative/administrative.module';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { ProductsModule } from './modules/products/products.module';
 import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
-  imports: [AdministrativeModule, DatabaseModule, AuthModule, CategoriesModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    AdministrativeModule,
+    DatabaseModule,
+    AuthModule,
+    CategoriesModule,
+    ProductsModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
