2e82150a40eb7993cbdd041fad602deda0b10c55
mtdias96 <matheusdias.front96@gmail.com>
2025-01-15 09:58:34 -0300

    feat: categoriesRepositories/add method findOne

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ActiveAdegaId } from 'src/shared/decorator/ActiveAdegaId';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { ListProductByCategoryDto } from './dto/list-product-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -18,5 +19,16 @@ export class CategoriesController {
   @Get()
   findAll(@ActiveAdegaId() adegaId: string) {
     return this.categoriesService.findAllByAdegaId(adegaId);
+  }
+
+  @Get('/:categoryId/products')
+  findOne(
+    @ActiveAdegaId() adegaId: string,
+    @Param() listProductByCategoryDto: ListProductByCategoryDto,
+  ) {
+    return this.categoriesService.listProductsByCategory(
+      adegaId,
+      listProductByCategoryDto,
+    );
   }
 }
