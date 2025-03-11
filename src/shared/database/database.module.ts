757696b4f7aa2a43085ba74ed9404f547f61da97
mtdias96 <matheusdias.front96@gmail.com>
2025-03-11 20:43:07 -0300

    feat: databaseModule/add finantial repository

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { CategoriesRepository } from './repositories/categories.repositories';
+import { FinancialRepository } from './repositories/financial.repositories';
 import { OrdersRepository } from './repositories/orders.repositories';
 import { ProductsRepository } from './repositories/products.repositories';
 import { StocksRepository } from './repositories/stock.repositories';
@@ -15,6 +16,7 @@ import { UsersRepository } from './repositories/users.repositories';
     ProductsRepository,
     StocksRepository,
     OrdersRepository,
+    FinancialRepository,
   ],
   exports: [
     UsersRepository,
@@ -22,6 +24,7 @@ import { UsersRepository } from './repositories/users.repositories';
     ProductsRepository,
     StocksRepository,
     OrdersRepository,
+    FinancialRepository,
   ],
 })
 export class DatabaseModule {}
