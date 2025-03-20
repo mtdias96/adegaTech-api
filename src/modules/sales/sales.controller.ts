0f72b1f38d60e8775eebf24db6603edefef09d35
mtdias96 <matheusdias.front96@gmail.com>
2025-03-20 09:02:17 -0300

    feat: salesModule/add new module sales

---
+++ src/modules/sales/sales.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { ActiveAdegaId } from 'src/shared/decorator/ActiveAdegaId';
+import { SalesService } from './sales.service';
+
+@Controller('sales')
+export class SalesController {
+  constructor(private readonly salesService: SalesService) {}
+
+  @Get('total-sales')
+  findAll(@ActiveAdegaId() adegaId: string) {
+    return this.salesService.findAll(adegaId);
+  }
+}
