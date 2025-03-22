32fddf549a7c7bd09f5e695949d7a014a28b7a28
mtdias96 <matheusdias.front96@gmail.com>
2025-03-22 09:09:08 -0300

    feat: latestModule/remove module latest

--- src/modules/latest-orders/latest-orders.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LatestOrdersController } from './latest-orders.controller';
-import { LatestOrdersService } from './latest-orders.service';
-
-@Module({
-  controllers: [LatestOrdersController],
-  providers: [LatestOrdersService],
-})
-export class LatestOrdersModule {}
