c6152596e25524d25477a1d4d70593e722f23fce
mtdias96 <matheusdias.front96@gmail.com>
2025-03-26 21:38:41 -0300

    feat: vercel/add new config 2

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
